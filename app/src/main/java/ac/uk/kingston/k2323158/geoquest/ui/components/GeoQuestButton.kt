03e4ff9193b2dad0039aff0f800fc6ecb5677700
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-04-08 00:27:41 +0100

    Fix ProfileScreen filename and move GeoQuestButton to ui/components

--- app/src/main/java/ac/uk/kingston/k2323158/geoquest/ui/components/GeoQuestButton.kt
+++ app/src/main/java/ac/uk/kingston/k2323158/geoquest/ui/components/GeoQuestButton.kt
@@ -3,7 +3,6 @@ package ac.uk.kingston.k2323158.geoquest.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
